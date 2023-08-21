a27a2ef74a85d967b0618f48e306ab5ae9f330d7
Jorge Cadavid <cadavidjorge10@gmail.com>
2023-08-21 18:01:49 -0500

    Add project files.

---
+++ libRnUniversidad_1/libRnUniversidad_1.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace libRnUniversidad_1
+
+{
+    public class libRnUniversidad_1
+    {
+        #region "Atributos"
+        private int intTipoEst;
+
+        private string strError;
+        #endregion
+
+        #region "Constructor"
+
+        #endregion
+
+        #region "Propiedades"
+        //ENTRADA
+        public int TipoEstudiante
+        {
+            set {intTipoEst = value;}  
+        }
+        //SALIDA
+        public string Error
+        { 
+            get { return strError; }
+        }
+        #endregion
+
+        #region "Metodos Privados"
+        private bool leerArchivo()
+        { 
+            if(!Validar())
+                return false;
+
+            try
+            { 
+                
+            }
+        }
+        #endregion
+
+        #region "Metodos Publicos"
+
+        #endregion
+    }
+}
